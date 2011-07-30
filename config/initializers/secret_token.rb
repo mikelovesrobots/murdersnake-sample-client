59ae77358708ed0ab863b0f1135ec88718d2baef
Mike Judge <mikelovesrobots@gmail.com>
2011-07-30 16:06:29 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MurdersnakeSampleClient::Application.config.secret_token = '00f496a6b0fe5895b9bb65b0ff60175a4309179f7fb4cf2de80c3f03b67fd1681a9ff1296a785d9cdf4617703b88f4ea2e42bc2c50520e52f2f05cd243df2f4f'
